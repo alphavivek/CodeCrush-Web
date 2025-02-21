128addca72236803635c3293790935de625e23e0
VivekKrPathak <Vkp10x27@gmail.com>
2025-02-21 19:33:34 +0530

    add redux toolkit + add constants to constants file + navbar dynamic

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,27 @@
-
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Body from './components/Body'
 import Profile from './components/Profile'
 import Login from './components/Login'
+import { Provider } from 'react-redux'
+import appStore from './utils/appStore'
+import Feed from './components/Feed'
 
 function App() {
 
   return (
     <>
-      <BrowserRouter basename='/'>
-        <Routes>
-          <Route path='/' element={<Body />}>
-            <Route path='/login' element={<Login />}></Route>
-            <Route path='/profile' element={<Profile />}></Route>
-          </Route>
-        </Routes>
-      </BrowserRouter>
-
-      {/* <Body /> */}
-      {/* <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1> */}
+      <Provider store={appStore}>
+        <BrowserRouter basename='/'>
+          <Routes>
+            <Route path='/' element={<Body />}>
+              <Route path='/' element={<Feed />}></Route>
+              <Route path='/login' element={<Login />}></Route>
+              <Route path='/profile' element={<Profile />}></Route>
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider> 
     </>
   )
 }
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,16 +1,26 @@
 import axios from 'axios';
 import React, { useState } from 'react'
+import { useDispatch } from 'react-redux';
+import { addUser } from '../utils/userSlice';
+import { useNavigate } from 'react-router-dom';
+import { BASE_URL } from '../utils/constants';
 
 const Login = () => {
     const [emailId, setEmailId] = useState('vivek@gmail.com');
     const [password, setPassword] = useState('Vivek@123');
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
 
     const handleLogin = async () => {
         try {
-            const res = await axios.post("http://localhost:3000/login", {
+            const res = await axios.post( BASE_URL+"/login", {
                 emailId,
                 password
-            });
+            },
+                { withCredentials: true }
+            );
+            dispatch(addUser(res.data));
+            navigate("/");
         } catch (err) {
             console.error(err);
         }
