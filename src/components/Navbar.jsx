e2ece840d1ecee3d38b27f3a7a92c8252f3eee69
VivekKrPathak <Vkp10x27@gmail.com>
2025-02-23 20:21:27 +0530

    add /profile/view api

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     const user = useSelector(store => store.user)
@@ -7,7 +8,7 @@ const Navbar = () => {
     return (
         <div className="navbar bg-base-300">
             <div className="flex-1">
-                <a className="btn btn-ghost text-xl">👩‍💻 CodeCrush</a>
+                <Link to={"/"} className="btn btn-ghost text-xl">👩‍💻 CodeCrush</Link>
             </div>
             {user && <div className="flex gap-2">
                 <p className='mt-1.5'>Welcome, {user.firstName}</p>
@@ -23,10 +24,10 @@ const Navbar = () => {
                         tabIndex={0}
                         className="menu menu-sm dropdown-content bg-base-200 rounded-box z-[1] mt-3 w-52 p-2 shadow">
                         <li>
-                            <a className="justify-between">
+                            <Link to={"/profile"} className="justify-between">
                                 Profile
                                 <span className="badge">New</span>
-                            </a>
+                            </Link>
                         </li>
                         <li><a>Settings</a></li>
                         <li><a>Logout</a></li>
